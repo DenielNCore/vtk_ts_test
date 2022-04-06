2e6d84dc8aa0e0938abe507b46e0131ee570e935
Denys Khlopov <denielncore@gmail.com>
2022-04-06 23:55:20 +0300

    add vtk

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+    'root'       : true,
+    'parser'     : 'vue-eslint-parser',
+    'extends'    : [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript/recommended',
+        '@vue/eslint-config-prettier',
+    ],
+    parserOptions: {
+        project: ['./tsconfig.json'],
+    },
+    'env'        : {
+        'vue/setup-compiler-macros': true,
+    },
+};
